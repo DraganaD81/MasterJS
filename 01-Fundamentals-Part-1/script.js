753686d5c8af3195d6c32e6b751417b3f8309db1
DraganaD81 <dragana@twognation.com>
2025-03-13 21:18:41 +0100

    The Complete JavaScript Course 2025 - 26. The Switch Statement and Challenge task

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -225,7 +225,6 @@ if (favourite === 23) {
 }
 
 if (favourite !== 23) console.log('Why not 23?');
-*/
 
 const hasDriverLicense = true; // A
 const hasGoodVision = true; // B
@@ -249,4 +248,48 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
   console.log('Sarah is able to drive!');
 } else {
   console.log('Someone else should drive...');
+}
+*/
+
+const day = 'friday';
+// console.log(typeof day);
+// console.log(typeof monday);
+
+switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+// Challenge
+
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
 }
