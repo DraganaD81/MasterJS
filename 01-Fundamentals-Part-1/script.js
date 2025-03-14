58c8b1fff876e6696c832ccdb1379916b07e82db
DraganaD81 <dragana@twognation.com>
2025-03-14 22:08:04 +0100

    The Complete JavaScript Course 2025 - 28. The Conditional (Ternary) Operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -292,7 +292,7 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
-*/
+
 
 3 + 4;
 1991;
@@ -304,3 +304,22 @@ if (23 > 10) {
 
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+*/
+
+const age = 23;
+// age >= 18
+//   ? console.log('I like to drink wine 🍷')
+//   : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
