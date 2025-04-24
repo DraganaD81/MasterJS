762508063b1dc7cb257be301fecd6940c9d639f2
DraganaD81 <dragana@twognation.com>
2025-04-24 17:30:45 +0200

    The Complete JS Course 2025 Section 7: 90. Resetting the Game - My solution

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -18,7 +18,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -79,4 +79,26 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  // console.log(activePlayer);
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  activePlayer = 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
 });
