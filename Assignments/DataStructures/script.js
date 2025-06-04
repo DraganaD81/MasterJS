1af9316cc97732ce1889b4f1ff71e67fb249b111
DraganaD81 <dragana@twognation.com>
2025-06-04 11:39:48 +0200

    The Complete JS Course 2025 Section 9: 112. Rest Pattern and Parameters/Assignment

--- Assignments/DataStructures/script.js
+++ Assignments/DataStructures/script.js
@@ -343,3 +343,25 @@ function spellWord(word) {
 }
 
 spellWord('JavaScript');
+
+// 4.1 Destructure the keywords property (array) of the first book from the books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, and the rest of the keywords should be assigned to the rest variable (it should be an array).
+
+const [mainKeyword, ...rest] = books[0].keywords;
+// console.log(books[0].keywords);
+// console.log(mainKeyword);
+// console.log(rest);
+
+// 4.2 Destructure the second book from the books array into a variable called bookPublisher. The bookPublisher variable should be assigned with the value of the publisher property of the book object. Assign the rest of the properties to the restOfTheBook variable.
+
+const { publisher: bookPublisher, ...restofTheBook } = books[1];
+// console.log(books[1].publisher);
+// console.log(books[1]);
+console.log(bookPublisher);
+
+// 4.3 Write a function called printBookAuthorsCount that has two parameters called title and authors. The authors parameter should accept any number of arguments. This function should log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".
+
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+
+function printBookAuthorsCount(title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
