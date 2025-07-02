4a5b90a4f7e23e57dfcad845f98779ea8f060394
DraganaD81 <dragana@twognation.com>
2025-07-02 13:12:30 +0200

    The Complete JS Course 2025 Section 9: 121. CHALLENGE #2/ My solution

--- Challenges/code.js
+++ Challenges/code.js
@@ -589,5 +589,73 @@ printGoals(...game.scored);
 //   }`
 // );
 
-team1 < team2 console.log('Team 1 is more likely to win')
-team1 > team2 console.log('Team 2 is more likely to win')
+// team1 < team2 console.log('Team 1 is more likely to win');
+// team1 > team2 console.log('Team 2 is more likely to win');
+
+// Coding Challenge #2
+
+/*
+Let's continue with our football betting app! Keep using the 'game' variable from
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console,
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them
+(except for "draw"). Hint: Note how the odds and the game objects have the
+same property names 😉
+4. Bonus: Create an object called 'scorers' which contains the names of the
+players who scored as properties, and the number of goals as the value. In this
+game, it will look like this:
+{
+Gnarby: 1,
+Hummels: 1,
+Lewandowski: 2
+}
+*/
+
+// 1.
+for (const [index, score] of Object.entries(game.scored)) {
+  console.log(`Goal ${Number(index) + 1}: ${score}`);
+}
+
+// 2.
+let averageOdd = 0;
+let numberOdd = 0;
+for (const odd of Object.values(game.odds)) {
+  numberOdd += 1;
+  averageOdd = (averageOdd + odd) / numberOdd;
+}
+console.log(`Average odd is: ${averageOdd}`);
+
+// 3.
+for (const [indexOdd, valueOdd] of Object.entries(game.odds)) {
+  if (indexOdd === 'team1') {
+    console.log(`Odd of victory ${game.team1}: ${valueOdd}`);
+  } else if (indexOdd === 'x') {
+    console.log(`Odd of draw: ${valueOdd}`);
+  } else if (indexOdd === 'team2') {
+    console.log(`Odd of victory ${game.team2}: ${valueOdd}`);
+  }
+
+  // console.log(indexOdd, valueOdd);
+}
+// console.log(Object.keys(game));
+
+// 4.
+const scorers = {};
+let goalNumbers = 1;
+for (const scorerName of Object.values(game.scored)) {
+  if (scorers[scorerName]) {
+    goalNumbers += 1;
+  } else goalNumbers = 1;
+  // console.log(scoredPlayer);
+  scorers[scorerName] = goalNumbers;
+  // console.log(scorerName, scorerIndex);
+}
+console.log(scorers);
