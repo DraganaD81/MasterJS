8520d4d6c24a993f88513fc6ff5ec5b30f3789c9
DraganaD81 <dragana@twognation.com>
2025-07-09 13:46:47 +0200

    The Complete JS Course 2025 Section 9: 122. Sets/Assignment

--- Assignments/DataStructures/script.js
+++ Assignments/DataStructures/script.js
@@ -518,3 +518,41 @@ const entries2 = Object.entries(books[0].thirdParty.goodreads);
 
 console.log(entries);
 console.log(entries2);
+
+// 12.1 Below is the allKeywords variable, which stores an empty array. Loop over the books array, and fill the allKeywords array with the keywords coming from the keywords property of each book object. The allKeywords array should have just one level (no nested arrays).
+
+// Use whatever loop and methods you want. You can also use the spread syntax. In the end, the allKeywords array should look more or less like this: ['computer science', 'programming', 'algorithms', 'data structures', ...].
+
+const allKeywords = [];
+
+for (const book of books) {
+  allKeywords.push(...book.keywords);
+}
+
+console.log(allKeywords);
+
+// 12.2 The allKeyword array contains duplicates. Remove them by creating a Set out of that array. Assign the newly created set to the uniqueKeywords variable.
+
+const uniqueKeywords = new Set(allKeywords);
+console.log(uniqueKeywords);
+
+// 12.3 Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'
+
+uniqueKeywords.add('coding');
+uniqueKeywords.add('science');
+console.log(uniqueKeywords);
+
+// 12.4 Delete 'business' from the uniqueKeywords set.
+
+uniqueKeywords.delete('business');
+console.log(uniqueKeywords);
+
+// 12.5 Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable.
+
+const uniqueKeywordsArr = [...uniqueKeywords];
+console.log(uniqueKeywordsArr);
+
+// 12.6 Delete all items from the uniqueKeywords set.
+
+uniqueKeywords.clear();
+console.log(uniqueKeywords);
