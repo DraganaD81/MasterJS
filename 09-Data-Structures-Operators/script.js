4598dcce8d9bd4ad16e83c3974847204644039f1
DraganaD81 <dragana@twognation.com>
2025-07-10 11:55:15 +0200

    The Complete JS Course 2025 Section 9: 123. New Operations to Make Sets Useful

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -48,6 +48,39 @@ const restaurant = {
   },
 };
 
+////////
+// New Operations to Make Sets Usefull
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+const commonFoods = italianFoods.intersection(mexicanFoods);
+console.log('Intersection', commonFoods);
+console.log([...commonFoods]);
+
+const italianMexicanFusion = italianFoods.union(mexicanFoods);
+console.log('Union', italianMexicanFusion);
+
+console.log([...new Set([...italianFoods, ...mexicanFoods])]);
+
+// Sets
+
+/*
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -70,8 +103,10 @@ ordersSet.delete('Risotto');
 console.log(ordersSet);
 
 for (const order of ordersSet) console.log(order);
+*/
 
 // Example
+/*
 const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
