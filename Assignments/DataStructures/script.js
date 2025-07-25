501d6edcd67b193594d608e9dfdc22e66034593c
DraganaD81 <dragana@twognation.com>
2025-07-25 18:01:01 +0200

    The Complete JS Course 2025 Section 9: 130. Working With Strings - Part 3/Assignment - My solution

--- Assignments/DataStructures/script.js
+++ Assignments/DataStructures/script.js
@@ -705,3 +705,59 @@ function logBookTheme(title) {
 //   console.log(i, books[i].title);
 //   logBookTheme(books[i].title);
 // }
+
+// 17.1 Below is the bookCategories variable that stores a string of categories. Each category is separated with a semicolon, for example, in a string "science;computing", 'science' and 'computing' are separate categories.
+
+// Write a function called logBookCategories that takes a string of categories separated with semicolons, and logs each category to the console (as separate strings).
+
+const bookCategories =
+  'science;computing;computer science;algorithms;business;operating systems;networking;electronics';
+
+const logBookCategories = function (categories) {
+  const books = categories.split(';');
+  console.log(...books);
+};
+
+logBookCategories(bookCategories);
+
+// 17.2 Now, the opposite. Each book from the books array has the keywords property.
+
+// Write a function called getKeywordsAsString that takes the books array as an argument, collects keywords from each book, removes duplicates, and then joins them to create a single string where keywords are separated by a semicolon.
+
+// console.log(books);
+
+const getKeywordsAsString = function (books) {
+  let allKeywords = [];
+  for (let i = 0; i < books.length; i++) {
+    allKeywords.push(...books[i].keywords);
+  }
+  // console.log(typeof keywords);
+  const uniqueKeywords = [...new Set(allKeywords)];
+  const keywordString = uniqueKeywords.join('; ');
+  console.log(keywordString);
+};
+
+getKeywordsAsString(books);
+
+// 17.3 Below is the bookChapters array that contains inner arrays. Each inner array consists of a chapter's title, and the number of a page, for example, in ['The Basics', 14], 'The Basics' is the chapter's title, and 14 is the number of a page.
+
+// Write a function called logBookChapters that takes an array of arrays (like bookChapters) as an argument, and logs each chapter's name to the console together with the page number. The page number should be separated from the chapter's name with underscores (take a look at the example below).
+
+// Use the padEnd method.
+
+const bookChapters = [
+  ['The Basics', 14],
+  ['Sorting', 254],
+  ['Searching', 372],
+  ['Graphs', 526],
+  ['Strings', 706],
+];
+
+const logBookChapters = function (chapters) {
+  for (let i = 0; i < chapters.length; i++) {
+    const key = chapters[i][0];
+    const value = chapters[i][1];
+    console.log(`${key.padEnd(15, '_')} ${value}`);
+  }
+};
+logBookChapters(bookChapters);
