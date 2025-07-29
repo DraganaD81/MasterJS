766d52af24c28bd631cdd8a5ebb1c25cba9f7023
DraganaD81 <dragana@twognation.com>
2025-07-30 00:15:01 +0200

    The Complete JS Course 2025 Section 9: 132. String Methods Practice

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -48,6 +48,28 @@ const restaurant = {
   },
 };
 
+// String practice
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(36);
+  console.log(output);
+}
+
+// Strings 3
+/*
 // Split and join
 console.log('a+very+nice+string'.split('+'));
 console.log('Jonas Schmedtman'.split(' '));
@@ -96,6 +118,7 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
+*/
 
 // Working with strings part 2
 /*
