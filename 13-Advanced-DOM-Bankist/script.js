b7a7a0f3a9c259d88ba2470461a4c2a4a59455bd
DraganaD81 <dragana@twognation.com>
2025-12-08 13:56:42 +0100

    The Complete JS Course 2025 Section 13: 202. Event Propagation in Practice - partial

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -178,3 +178,10 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 */
 
 // Event Propagation: Bubbling and Capturing
+
+// rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
