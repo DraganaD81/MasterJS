f75eec19f5c2683fd55b67317c9fa00b2b2bf2ec
DraganaD81 <dragana@twognation.com>
2026-02-19 14:36:22 +0100

    The Complete JS Course 2025 Section 16: 266. Handling Rejected Promises

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -3,6 +3,27 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function(msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+}
+
+const renderCountry = function(data, className = '') {
+  const html = `
+  <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>`;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        // countriesContainer.style.opacity = 1;
+};
+
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -43,21 +64,7 @@ getCountryData('germany');
 
 /* Callback*/
 
-const renderCountry = function(data, className = '') {
-  const html = `
-  <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-          </div>
-        </article>`;
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
-}
+
 /*
 const getCountryAndNeighbour = function(country) {
 const request = new XMLHttpRequest();
@@ -119,10 +126,13 @@ setTimeout(() => {
 //   })
 // };
 
+
+
 const getCountryData = function(country) {
   // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
-  .then(response => response.json())
+  .then(response => 
+    response.json())
   .then(data => {
     renderCountry(data[0]);
     const neighbour = data[0].borders[0]
@@ -131,13 +141,20 @@ const getCountryData = function(country) {
 
     // Country 2
     return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-    // return 23;
 })
 .then(response => response.json())
 .then(data => renderCountry(data, 'neighbour'))
-// .then(data => alert(data));
+.catch(err => {
+  console.error(`${err} 💥💥💥`);
+  renderError(`Something went wrong 💥💥${err.message}. Try again!`)
+})
+.finally(() => {
+    countriesContainer.style.opacity = 1;
+});
 };
 
-// getCountryData('portugal');
-getCountryData('germany');
-// getCountryData('serbia');
+btn.addEventListener('click', function() {
+  getCountryData('portugal');
+});
+
+  getCountryData('dudhdhas');
