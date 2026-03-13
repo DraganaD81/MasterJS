38ce460c9aacad0bf3370ddf1bfc09bfb17a85f6
DraganaD81 <dragana@twognation.com>
2026-03-13 21:29:38 +0100

    The Complete JS Course 2025 Section 16: 275. Error Handling With try...catch

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -523,21 +523,39 @@ const getPosition = function() {
 // fetch(`https://restcountries.com/v2/name/${country}`).then(res => console.log(res))
 
 const whereAmI = async function() {
+  try {
   // Geolocation
   const pos = await getPosition();
   const {latitude: lat, longitude: lng} = pos.coords;
   
   // Reverse geocoding
   const resGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
+    if(!resGeo.ok) throw new Error('Problem getting location data')
+
     const dataGeo = await resGeo.json();
     console.log(dataGeo);
 
   // Country data
   const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.countryCode}`);
+  if(!res.ok) throw new Error('Problem getting country')
+
   const data = await res.json()
   console.log(data);
-  renderCountry(data[6]);
+  renderCountry(data[6]);} catch(err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+  }
 
 }
 whereAmI();
+whereAmI();
+whereAmI();
 console.log('FIRST');
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   y = 3;
+// } catch (err) {
+//   alert(err.message)
+// }
