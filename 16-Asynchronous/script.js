6058064a31d4323e0e0562448e988345c97288bb
DraganaD81 <dragana@twognation.com>
2026-03-13 20:49:49 +0100

    The Complete JS Course 2025 Section 16: 274. Consuming Promises with Async/Await

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -466,7 +466,8 @@ wait()
 //   });
 // }
 
-*/
+// FINAL SOLUTION
+
 const wait = function(seconds) {
   return new Promise(function(resolve) {
     setTimeout(resolve, seconds * 1000)
@@ -511,3 +512,32 @@ createImage('img/img-1.jpg').then(img => {
   currentImg.style.display = 'none';
 })
 .catch(err => console.error(err));
+*/
+
+const getPosition = function() {
+  return new Promise(function(resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+// fetch(`https://restcountries.com/v2/name/${country}`).then(res => console.log(res))
+
+const whereAmI = async function() {
+  // Geolocation
+  const pos = await getPosition();
+  const {latitude: lat, longitude: lng} = pos.coords;
+  
+  // Reverse geocoding
+  const resGeo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+
+  // Country data
+  const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.countryCode}`);
+  const data = await res.json()
+  console.log(data);
+  renderCountry(data[6]);
+
+}
+whereAmI();
+console.log('FIRST');
