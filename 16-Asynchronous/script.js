f6065b03ca1f543ec8187f65956e672fc62425c0
DraganaD81 <dragana@twognation.com>
2026-03-17 15:56:39 +0100

    The Complete JS Course 2025 Section 16: 277. Running Promises in Parallel

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -24,6 +24,17 @@ const renderCountry = function(data, className = '') {
         countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+  return fetch(url)
+  .then(response => {
+
+    if(!response.ok)
+      throw new Error(`${errorMsg} (${response.status})`)
+
+    return response.json();
+  });
+};
+
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -512,7 +523,7 @@ createImage('img/img-1.jpg').then(img => {
   currentImg.style.display = 'none';
 })
 .catch(err => console.error(err));
-*/
+
 
 const getPosition = function() {
   return new Promise(function(resolve, reject) {
@@ -568,3 +579,28 @@ console.log('1. Will get location');
   }
   console.log('3. Finished getting location')
 })();
+*/
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([getJSON(
+      `https://restcountries.com/v2/name/${c1}`
+    ), getJSON(
+      `https://restcountries.com/v2/name/${c2}`
+    ), getJSON(
+      `https://restcountries.com/v2/name/${c3}`
+    )
+  ]);
+  console.log(data.map(d => d[0].capital));
+
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+get3Countries('portugal', 'canada', 'tanzania')
